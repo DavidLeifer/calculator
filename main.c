344e255d38915c8fca7381058f066b5f7c8d371e
David <142959735+DavidLeifer@users.noreply.github.com>
2026-07-03 08:40:06 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,40 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
 #include "./src/struct.h"
 #include "./src/utility.h"
 #include "./src/binaryChar.h"
 #include "./src/arithmatic.h"
+#include "./src/arithmaticSteps.h"
 #include "./src/userInput.h"
 #include "./src/interface.h"
-
 /*
 Notes
-
-  Have to specify function calls in binaryFunctions.h and here besides the
-  actual binaryFunctions.c file.
-
-  // listing all the files: gcc main.c binaryFunctions.c arithmatic.c userInput.c -o binary && ./binary
-
+// listing all the files: gcc main.c binaryFunctions.c arithmatic.c userInput.c -o binary && ./binary
 gcc main.c ./src/*.c -o binary && ./binary
-
 todo
-
-  - char[] subtraction, multiplication, division.
-
+  - multiplication, division.
 minor todo
-
   - char length function.
   - fix inputArithmatic variable names with new struct int pointers and malloc.
   - use 'maxBinaryLength' in "binaryChar.c"
-
+  - subtraction() and addition() use same function series to convert int to char[] binary.
+  - test 'fileLog()' error messages from 'subtraction()' and 'binaryInvert()'.
 */
-
+// 'version' and 'maxBinaryLength' declared in "binaryChar.h"
+// 'version' and 'features' declared in the file "interface.h"
 const char version[24] = "Calculator\nVersion 0.03";
 const char features[44] = "Features: addition, input logging, testing.";
-
-// This would be used in "binaryChar.c" 'binaryAddition()'
+// This is used in "binaryChar.c" 'binaryAddition()'
 const int maxBinaryLength = 17;
 
 int main(int argc, char *argv[4]) {
