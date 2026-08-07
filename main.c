b3ebbd706b998f527628787a6fbed52cf6d4eff8
David <142959735+DavidLeifer@users.noreply.github.com>
2026-08-07 14:19:44 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -31,9 +31,15 @@ Notes
    the 'const' is set as another variable with 'malloc()' or 'while{}' even if it's a
    pointer. The other function has to already accept a 'const' which limits basic
    functions from "utility.c".
+4) 20260726 - 'char' pointer with 'malloc()' doesn't need 'strcpy(dest, "a")' unlike
+              previous versions. It also doesn't fill the array with duplicate "a" the
+              length of the 'malloc()' 'char' pointer since 'memset()' suddenly exists.
+            - 'charAppend()' in "utility.c" 'strcpy()' was commented out but it might break stuff later.
 
 todo
   - multiplication, division.
+    - line 521 - 584: reduce times this is set:
+      diffrence 'intBinaryGreatTwo.two = 1' and (first input is longer) 'intBinaryGreatTwo.three = 1'
   - "window.c" GUI.
 
 minor todo
@@ -41,6 +47,25 @@ minor todo
   - subtraction() and addition() use same function series to convert int to char[] binary.
   - test 'fileLog()' error messages from 'subtraction()' and 'binaryInvert()'.
   - 'serverConnect()' in "window.c" has a bitwise comparison that's mostly in "binaryChar.c".
+  - 'multiplication()' in "arithmaticSteps.c"
+    - Probably don't need 'binaryAddCheck' for the if/else since 'intBinaryGreatTwo.three' says
+      which one is longer with '0' '1' '2' but the extra int is four extra bytes.
+    - 'binaryAddition()' in "arithmatic.c" uses a 'freeCheck' to 'free()' the inputs and the "arithmatic.c".
+      functions were changed. The deceased 'binaryAdditionTwo()' used 'free()' on the second input char[] pointer
+      since 'struct binaryAdd' is both the input and the output. An 'int' check of '0' '1' '2' '3'
+      is used to free in the first 'binaryAddition()', removing 'binaryAdditionTwo()' but doesn't
+      reduce heap usage (actually increases stack memory by (4 * the number) of times it's used throughout
+      // the entire program (~4-6 times)).
+      // might not need to return the length of the char pointer or use a struct at all
+      // not sure if the struct is too large since its only a 'int' a 'char pointer', and two blank 'int'.
+
+      //struct threeInt intBinaryGreat = intBinaryCheck(intBinaryOne, intBinaryTwo);
+      /////////////////////////////////////////////////////
+      // or use the function since 'struct threeInt intBinaryGreatThree' has a blank char[] pointer on heap the entire while loop
+      // in addition to 'struct threeInt binaryAdd' which also exists the entire time.
+      // 'binaryAdd.string' and either 'zeroStep' or 'oneStep' are the same length
+
+
 */
 // 'version' and 'maxBinaryLength' declared in "binaryChar.h"
 // 'version' and 'features' declared in the file "interface.h"
@@ -63,8 +88,16 @@ int main(int argc, char *argv[4]) {
   // Long print tests addition but 'interface()' expects a length of 0-4 char.
   // printf("Addition tests:\n");
   // int intInterface = interface(argc, "-lt\0");
-
-  // The previous CLI // The calculator text GUI is shown if terminal doesn't ask for help.
+  //int testOne = 21; // last row has a '?' at the end
+  //int testTwo = 10;
+  //int testOne = 15;
+  //int testTwo = 11;
+  //int testOne = 1;
+  //int testTwo = 9;
+  //int outputMultiplication =  multiplication(testOne, testTwo);
+  // The CLI
+  // The calculator text GUI is shown if terminal doesn't ask for help.
+  // change to ask for '-c' or something similar after the GUI.
   if (argc == 1) {
     textUI();
     return 1;
